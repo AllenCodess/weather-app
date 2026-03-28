304d75a5540f8f38932eaa76863e04a7ebab3f09
AllenCodess <Aph296@protonmail.com>
2026-03-28 17:27:20 -0400

    successfully fetching data from API

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const apiKey = process.env.weatherApiKey;
+const apiKey = "11d9a9a0c8e6463287c63348262703";
+async function fetchAPIData() {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Tampa&days=3&aqi=no&alerts=no`,
+  );
+  const data = await response.json();
+
+  console.log(data);
+}
+
+fetchAPIData();
